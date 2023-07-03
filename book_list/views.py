8a972541b2bbeabd8cc4ee92513f71a9da466554
AluaBekzat <138497681+AluaBekzat@users.noreply.github.com>
2023-07-03 20:19:36 +0600

    update

--- book_list/views.py
+++ book_list/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from product_list.models import Product, Category
+
+def all_book(request):
+    book = Product.objects.all()
+    genre = Category.objects.all()
+    context = {
+        'book' : book,
+        'genre' : genre
+    }
+    return render (request, 'main.html', context)
--- product_list/views.py
+++ product_list/views.py
@@ -20,13 +20,5 @@ def products_on_category(request, pk):
     }
     return render(request, 'main.html', context)
 
-from product_list.models import Product, Category
-
-def all_book(request):
-    book = Product.objects.all()
-    genre = Category.objects.all()
-    context = {
-        'book' : book,
-        'genre' : genre
-    }
-    return render (request, 'main.html', context)
+def querty (request):
+    return render(request, '123.html')
